f1190722de0289914e5fc84de64bc37d84ae844c
sigitwijk16 <107915024+sigitwijk16@users.noreply.github.com>
2023-08-18 04:43:23 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg': '#302B4E',
+      },
+    },
+  },
+  plugins: [],
+}
+
